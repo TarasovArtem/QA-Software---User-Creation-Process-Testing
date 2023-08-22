9cbfa5c50a5fe46490839f9eef34dee3c6175dfc
Artem <hans8883@yandex.ru>
2023-08-22 16:01:03 +0200

    settings: add configuration for tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl:"http://localhost:80",
+    execTimeout: 300000,
+      defaultCommandTimeout: 60000,
+      pageLoadTimeout: 60000,
+      viewportWidth: 1920,
+      viewportHeight: 1080,
+      requestTimeout: 5000,
+      responseTimeout: 60000,
+      retries: {
+        runMode: 2,
+        openMode: 1
+      },
+      failOnStatusCode: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
