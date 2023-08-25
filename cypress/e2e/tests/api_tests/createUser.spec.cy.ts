3b2485e3f43a1d119cc37434088bda9f4b374fbd
Artem <hans8883@yandex.ru>
2023-08-25 16:57:01 +0200

    test: add request with empty data

--- cypress/e2e/tests/api_tests/createUser.spec.cy.ts
+++ cypress/e2e/tests/api_tests/createUser.spec.cy.ts
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
- 
+
 
 describe("Create User", () => {
 
@@ -28,7 +28,10 @@ describe("Create User", () => {
         }
     };
 
+    
+
     it("Mutation - Create User", () => {
+      
         cy.request({
             method: 'POST',
             url: 'http://localhost:8080/graphql',
@@ -54,11 +57,66 @@ describe("Create User", () => {
                 console.error('Request failed with status:', response.status);
               }
         });
-    })
+    })   
 })
 
 
+describe("Create User - Empty Data", () => {
+  
+  const query = `
+  mutation CreateUserMutation($input: CreateUserInput!) {
+      createUser(input: $input) {
+          ok
+          error
+          user {
+          uuid
+          email
+          firstName
+          lastName
+          newsletter
+          }
+      }
+  }
+  `;
+  //The data can be random (Math.random())
+  const variables = {
+      input: {
+      "email": "",
+      "firstName": "",
+      "lastName": "",
+      "newsletter": false
+      }
+  };
 
+  it("Mutation - Create User with empty data", () => {
+      cy.request({
+          method: 'POST',
+          url: 'http://localhost:8080/graphql',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: {
+            query,
+            variables,
+          },
+      })
+      .then(response => {
+        // Verify that the request returned a successful status and the expected results
+          if (response.status === 200) {
+              const data = response.body;
+              //console.log(data);
+              cy.log(data);
+              expect(data.data.createUser.error).equal("INVALID_EMAIL");
+              expect(data.data.createUser.ok).equal(false);
+              // incorrect result --- users: null. 
+              //The data has been sent, but the user has not been created. Missing table in DB
+              cy.log(data.data.createUser);
+            } else {
+              console.error('Request failed with status:', response.status);
+            }
+      });
+  })   
+})
 
 
 
